5b7d51842afee7cd5f6ff8364ef170ce74fffb71
rphi <8237136+rphi@users.noreply.github.com>
2019-02-24 15:06:09 +0000

    Append to req_options not overwrite

--- api/app.py
+++ api/app.py
@@ -5,12 +5,10 @@ from endpoints import default, init, position
 
 # falcon.API instances are callable WSGI apps, initialise the app
 HardwareAPI = api = falcon.API()
-api.req_options = {
-    'initialised': False,
-    'Xlength': 100,
-    'Ylength': 100,
-    'gripper': 'irobot'
-}
+api.req_options['initialised'] = False
+api.req_options['Xlength'] = 100
+api.req_options['Ylength'] = 100
+api.req_options['gripper'] = 'irobot'
 
 # define your routes
 api.add_route('/', default.DefaultResource())
